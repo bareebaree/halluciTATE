b0c0d5e457b3d455b646751780f5870db12e044d
James Tate <james_tate@ymail.com>
2025-08-20 17:44:47 +0100

    Update __main__.py
    Updated to add reference to clustering step

--- pipeline/__main__.py
+++ pipeline/__main__.py
@@ -2,18 +2,24 @@
 
 import argparse
 from pipeline.embed import embed_fasta
+from pipeline.cluster import run_clustering_and_analysis
 
 def main():
-    parser = argparse.ArgumentParser(description="Protein ESM2 embedding pipeline")
+    parser = argparse.ArgumentParser(description="Protein pipeline")
     parser.add_argument("--family", type=str, required=True, help="Protein family name")
+    parser.add_argument("--stage", choices=["embed", "cluster", "all"], default="all",
+                        help="Which pipeline stage to run")
+
     args = parser.parse_args()
-
     protein_family = args.family
-    fasta_path = f"./data/initial_proteins/{protein_family}/combined_{protein_family}s.fasta"
-    embedding_output_path = f"./data/initial_proteins/{protein_family}/{protein_family}_esm2_embeddings.npz"
 
-    embed_fasta(fasta_path, output_path)
+    if args.stage in ["embed", "all"]:
+        fasta_path = f"./data/initial_proteins/{protein_family}/combined_{protein_family}s.fasta"
+        embedding_output_path = f"./data/initial_proteins/{protein_family}/{protein_family}_esm2_embeddings.npz"
+        embed_fasta(fasta_path, embedding_output_path)
+
+    if args.stage in ["cluster", "all"]:
+        run_clustering_and_analysis(protein_family)
 
 if __name__ == "__main__":
     main()
-
